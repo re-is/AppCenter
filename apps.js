ff08bd8973bb0ba39de861f0511d6300dd18c763
re-is <33784291+re-is@users.noreply.github.com>
2019-12-26 16:34:56 +0100

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,27 @@
+window.usbApps = {
+	'Google Chrome': ['12.25', 'https://www.dropbox.com/s/v8g9ot3ar4xab8e/Any.zip?dl=0'],
+	'DirectX9': [],
+	'ESET Kódmásoló': [],
+	'ESET Smart Security': [],
+	'Firefox': [],
+	'Office 2016': [],
+	'Realtek HD Audio': [],
+	'VLC': [],
+	'Intel HD 5500': []
+};
+
+window.pcApps = {
+	'Any To Icon': ['12.25', 'https://www.dropbox.com/s/v8g9ot3ar4xab8e/Any.zip?dl=0'],
+	'AutoIt': [],
+	'CCleaner': [],
+	'Defrag': [],
+	'KeepAwake': [],
+	'Photo Filter 10': [],
+	'Resource Hacker': [],
+	'Sublime Text 3': [],
+	'uTorrent': [],
+	'Volumouse': [],
+	'WinRAR': [],
+	'WinSCP': [],
+	'WU Switcher': []
+};
