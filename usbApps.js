fc6ea9e7332c4d02c190212d25c39e7757a92ea8
re-is <33784291+re-is@users.noreply.github.com>
2019-12-26 09:54:53 +0100

    Add files via upload

---
+++ usbApps.js
@@ -0,0 +1,11 @@
+window.usbApps = {
+	'Google Chrome': ['https://www.dropbox.com/s/v8g9ot3ar4xab8e/Any.zip?dl=0', '12.25'],
+	'DirectX9': [],
+	'ESET Kódmásoló': [],
+	'ESET Smart Security': [],
+	'Firefox': [],
+	'Office 2016': [],
+	'Realtek HD Audio': [],
+	'VLC': [],
+	'Intel HD 5500': []
+};
