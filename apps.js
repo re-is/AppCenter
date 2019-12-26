b97dca4e2642296075c8e68bb2e9993d97c0c3b6
re-is <33784291+re-is@users.noreply.github.com>
2019-12-26 18:35:19 +0100

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,28 +1,27 @@
 window.usbApps = {
-	'Intel HD 5500': [],
-	'DirectX9': [],
-	'Google Chrome': ['12.25', 'https://www.dropbox.com/s/v8g9ot3ar4xab8e/Any.zip?dl=0'],
-	'Firefox': [],
-	'Realtek HD Audio': [],
-	'VLC': [],
-	'Office 2016': [],
-	'ESET Kódmásoló': [],
-	'ESET Smart Security': []
-	
+	'Intel HD 5500': ['12.26', 'https://www.dropbox.com/s/'],
+	'DirectX9': ['12.26', 'https://www.dropbox.com/s/'],
+	'Google Chrome': ['12.26', 'https://www.dropbox.com/s/'],
+	'Firefox': ['12.26', 'https://www.dropbox.com/s/'],
+	'Realtek HD Audio': ['12.26', 'https://www.dropbox.com/s/'],
+	'VLC': ['12.26', 'https://www.dropbox.com/s/'],
+	'Office 2016': ['12.26', 'https://www.dropbox.com/s/'],
+	'ESET Kódmásoló': ['12.26', 'https://www.dropbox.com/s/'],
+	'ESET Smart Security': ['12.26', 'https://www.dropbox.com/s/']
 };
 
 window.pcApps = {
-	'Any To Icon': ['12.25', 'https://www.dropbox.com/s/v8g9ot3ar4xab8e/Any.zip?dl=0'],
-	'AutoIt': [],
-	'CCleaner': [],
-	'Defrag': [],
-	'KeepAwake': [],
-	'Photo Filter 10': [],
-	'Resource Hacker': [],
-	'Sublime Text 3': ['12.25', 'https://www.dropbox.com/s/ql09a3tla6hysyw/Sublime.zip?dl=0'],
-	'uTorrent': [],
-	'Volumouse': [],
-	'WinRAR': [],
-	'WinSCP': [],
-	'WU Switcher': []
+	'Any To Icon': ['12.26', 'https://www.dropbox.com/s/v8g9ot3ar4xab8e/Any.zip?dl=0'],
+	'AutoIt': ['12.26', 'https://www.dropbox.com/s/'],
+	'CCleaner': ['12.26', 'https://www.dropbox.com/s/'],
+	'Defrag': ['12.26', 'https://www.dropbox.com/s/'],
+	'KeepAwake': ['12.26', 'https://www.dropbox.com/s/'],
+	'Photo Filter 10': ['12.26', 'https://www.dropbox.com/s/'],
+	'Resource Hacker': ['12.26', 'https://www.dropbox.com/s/'],
+	'Sublime Text 3': ['12.26', '12.26', 'https://www.dropbox.com/s/'],
+	'uTorrent': ['12.26', 'https://www.dropbox.com/s/'],
+	'Volumouse': ['12.26', 'https://www.dropbox.com/s/'],
+	'WinRAR': ['12.26', 'https://www.dropbox.com/s/'],
+	'WinSCP': ['12.26', 'https://www.dropbox.com/s/'],
+	'WU Switcher': ['12.26', 'https://www.dropbox.com/s/']
 };
