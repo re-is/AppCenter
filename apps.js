40d315fb89aaff81823575fd4dfc8782cbd3bc66
re-is <33784291+re-is@users.noreply.github.com>
2019-12-26 17:15:32 +0100

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,13 +1,14 @@
 window.usbApps = {
-	'Google Chrome': ['12.25', 'https://www.dropbox.com/s/v8g9ot3ar4xab8e/Any.zip?dl=0'],
+	'Intel HD 5500': [],
 	'DirectX9': [],
-	'ESET Kódmásoló': [],
-	'ESET Smart Security': [],
+	'Google Chrome': ['12.25', 'https://www.dropbox.com/s/v8g9ot3ar4xab8e/Any.zip?dl=0'],
 	'Firefox': [],
-	'Office 2016': [],
 	'Realtek HD Audio': [],
 	'VLC': [],
-	'Intel HD 5500': []
+	'Office 2016': [],
+	'ESET Kódmásoló': [],
+	'ESET Smart Security': []
+	
 };
 
 window.pcApps = {
@@ -18,7 +19,7 @@ window.pcApps = {
 	'KeepAwake': [],
 	'Photo Filter 10': [],
 	'Resource Hacker': [],
-	'Sublime Text 3': [],
+	'Sublime Text 3': ['12.25', 'https://www.dropbox.com/s/ql09a3tla6hysyw/Sublime.zip?dl=0'],
 	'uTorrent': [],
 	'Volumouse': [],
 	'WinRAR': [],
