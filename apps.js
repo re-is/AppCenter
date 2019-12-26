34c2ec67dbc84ba43ad4af2b7c19c0b5f9e6161c
re-is <33784291+re-is@users.noreply.github.com>
2019-12-26 19:41:23 +0100

    Update apps.js

--- apps.js
+++ apps.js
@@ -11,16 +11,16 @@ window.usbApps = {
 };
 
 window.pcApps = {
-	'Any To Icon': ['12.26', 'https://www.dropbox.com/s/'],
-	'AutoIt': ['12.26', 'https://www.dropbox.com/s/'],
-	'CCleaner': ['12.26', 'https://www.dropbox.com/s/'],
-	'Defrag': ['12.26', 'https://www.dropbox.com/s/'],
-	'KeepAwake': ['12.26', 'https://www.dropbox.com/s/'],
-	'Photo Filter 10': ['12.26', 'https://www.dropbox.com/s/'],
-	'Resource Hacker': ['12.26', 'https://www.dropbox.com/s/'],
+	'Any To Icon': ['12.26', 'https://www.dropbox.com/s/38n59xa8x6ai53t/Any-12.26.zip?dl=0'],
+	'AutoIt': ['12.26', 'https://www.dropbox.com/s/1gf6wl8s6ay8lmf/Autoit-12.26.zip?dl=0'],
+	'CCleaner': ['12.26', 'https://www.dropbox.com/s/c0qym4hupunxen9/CCleaner-12.26.zip?dl=0'],
+	'Defrag': ['12.26', 'https://www.dropbox.com/s/4z7rxdywg9t80h6/Defrag-12.26.zip?dl=0'],
+	'KeepAwake': ['12.26', 'https://www.dropbox.com/s/ymxmr5iakam7tbn/KeepAwake-12.26.zip?dl=0'],
+	'Photo Filter 10': ['12.26', 'https://www.dropbox.com/s/j1egsehzm988xj1/Photo-12.26.zip?dl=0'],
+	'Resource Hacker': ['12.26', 'https://www.dropbox.com/s/a18w9e3xhfxjw2g/ResHacker-12.26.zip?dl=0'],
 	'Sublime Text 3': ['12.26', 'https://www.dropbox.com/s/gt6i8nvdnwoies7/SublimeText-12.26.zip?dl=0'],
-	'uTorrent': ['12.26', 'https://www.dropbox.com/s/'],
-	'Volumouse': ['12.26', 'https://www.dropbox.com/s/'],
+	'uTorrent': ['12.26', 'https://www.dropbox.com/s/206iqnpdicgo1rc/uTorrent-12.26.zip?dl=0'],
+	'Volumouse': ['12.26', 'https://www.dropbox.com/s/cp6el4g2bbhzob8/Volumouse-12.26.zip?dl=0'],
 	'WinRAR': ['12.26', 'https://www.dropbox.com/s/'],
 	'WinSCP': ['12.26', 'https://www.dropbox.com/s/'],
 	'WU Switcher': ['12.26', 'https://www.dropbox.com/s/']
