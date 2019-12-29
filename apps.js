d76c15f564db58bdc3e20f22a2728780776802b3
re-is <33784291+re-is@users.noreply.github.com>
2019-12-29 11:58:34 +0100

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,27 +1,27 @@
 window.usbApps = {
 	'Intel HD 5500': ['', 'offline'],
 	'DirectX': ['9.c', 'offline'],
-	'Google Chrome': ['', 'https://github.com/re-is/AppCenter/raw/master/chrome.zip'],
-	'Firefox': ['', 'https://github.com/re-is/AppCenter/raw/master/firefox.zip'],
+	'Google Chrome': ['', 'chrome.zip'],
+	'Firefox': ['', 'firefox.zip'],
 	'Realtek HD Audio': ['2.82', 'offline'],
 	'VLC': ['3.0.4', 'offline'],
 	'Office': ['2016', 'offline'],
-	'ESET Kódmásoló': ['12.27', 'https://github.com/re-is/AppCenter/raw/master/eset-copier.zip'],
-	'ESET Smart Security': ['', 'https://github.com/re-is/AppCenter/raw/master/eset.zip']
+	'ESET Kódmásoló': ['12.27', 'eset-copier.zip'],
+	'ESET Smart Security': ['', 'eset.zip']
 };
 
 window.pcApps = {
-	'Any To Icon': ['12.26', 'https://github.com/re-is/AppCenter/raw/master/Any-12.26.zip'],
-	'AutoIt': ['12.26', 'https://github.com/re-is/AppCenter/raw/master/Autoit-12.26.zip'],
-	'CCleaner': ['12.26', 'https://github.com/re-is/AppCenter/raw/master/CCleaner-12.26.zip'],
-	'Defrag': ['12.26', 'https://github.com/re-is/AppCenter/raw/master/Defrag-12.26.zip'],
-	'KeepAwake': ['12.26', 'https://github.com/re-is/AppCenter/raw/master/KeepAwake-12.26.zip'],
-	'Photo Filter 10': ['12.26', 'https://github.com/re-is/AppCenter/raw/master/Photo-12.26.zip'],
-	'Resource Hacker': ['12.26', 'https://github.com/re-is/AppCenter/raw/master/ResHacker-12.26.zip'],
-	'Sublime Text 3': ['12.26#2', 'https://github.com/re-is/AppCenter/raw/master/'],
-	'uTorrent': ['12.26', 'https://github.com/re-is/AppCenter/raw/master/uTorrent-12.26.zip'],
-	'Volumouse': ['12.26', 'https://github.com/re-is/AppCenter/raw/master/Volumouse-12.26.zip'],
-	'WinRAR': ['12.26', 'https://github.com/re-is/AppCenter/raw/master/Win-Rar-12.26.zip'],
-	'WinSCP': ['12.26', 'https://github.com/re-is/AppCenter/raw/master/WinScp-12.26.zip'],
-	'WU Switcher': ['12.26', 'https://github.com/re-is/AppCenter/raw/master/wu-12.26.zip']
+	'Any To Icon': ['12.26', 'Any-12.26.zip'],
+	'AutoIt': ['12.26', 'Autoit-12.26.zip'],
+	'CCleaner': ['12.26', 'CCleaner-12.26.zip'],
+	'Defrag': ['12.26', 'Defrag-12.26.zip'],
+	'KeepAwake': ['12.26', 'KeepAwake-12.26.zip'],
+	'Photo Filter 10': ['12.26', 'Photo-12.26.zip'],
+	'Resource Hacker': ['12.26', 'ResHacker-12.26.zip'],
+	'Sublime Text 3': ['12.26#2', ''],
+	'uTorrent': ['12.26', 'uTorrent-12.26.zip'],
+	'Volumouse': ['12.26', 'Volumouse-12.26.zip'],
+	'WinRAR': ['12.26', 'Win-Rar-12.26.zip'],
+	'WinSCP': ['12.26', 'WinScp-12.26.zip'],
+	'WU Switcher': ['12.26', 'wu-12.26.zip']
 };
