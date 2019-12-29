e9b290f4d9eafa40f337388475278c79473d1444
re-is <33784291+re-is@users.noreply.github.com>
2019-12-29 15:43:59 +0100

    Update apps.js

--- apps.js
+++ apps.js
@@ -11,14 +11,14 @@ window.usbApps = {
 };
 
 window.pcApps = {
-	'Any To Icon': ['12.26', 'Any-12.26.zip', 'dfsf sdfsdf.zip', '33333.zip'],
+	'Any To Icon': ['12.26', 'Any-12.26.zip'],
 	'AutoIt': ['12.26', 'Autoit-12.26.zip'],
 	'CCleaner': ['12.26', 'CCleaner-12.26.zip'],
 	'Defrag': ['12.26', 'Defrag-12.26.zip'],
 	'KeepAwake': ['12.26', 'KeepAwake-12.26.zip'],
 	'Photo Filter 10': ['12.26', 'Photo-12.26.zip'],
 	'Resource Hacker': ['12.26', 'ResHacker-12.26.zip'],
-	'Sublime Text 3': ['12.26', 'aa', 'bb'],
+	'Sublime Text 3': ['12.26', 'Sublime-12.29.zip'],
 	'uTorrent': ['12.26', 'uTorrent-12.26.zip'],
 	'Volumouse': ['12.26', 'Volumouse-12.26.zip'],
 	'WinRAR': ['12.26', 'Win-Rar-12.26.zip'],
