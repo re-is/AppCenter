683d41e25fc9384f7def032bc50b17ced6645ca3
re-is <33784291+re-is@users.noreply.github.com>
2020-11-21 18:59:53 +0100

    Update apps.js

--- apps.js
+++ apps.js
@@ -18,7 +18,7 @@ window.pcApps = {
 	'KeepAwake': ['12.26', 'KeepAwake-12.31.zip'],
 	'Photo Filter 10': ['12.26', 'Photo-12.26.zip'],
 	'Resource Hacker': ['12.26', 'ResHacker-12.26.zip'],
-	'Sublime Text 3': ['11.15', 'Sublime-11.15.zip'],
+	'Sublime Text 3': ['11.21', 'Sublime-11.21.zip'],
 	'uTorrent': ['11.15', 'uTorrent-11.15.zip'],
 	'Volumouse': ['12.26', 'Volumouse-12.26.zip'],
 	'WinRAR': ['12.26', 'Win-Rar-12.26.zip'],
