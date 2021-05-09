6c302b2af7f61e4cb818aa66d39ed9154e6a0c35
re-is <33784291+re-is@users.noreply.github.com>
2021-05-09 17:26:06 +0200

    Update apps.js

--- apps.js
+++ apps.js
@@ -5,7 +5,7 @@ window.usbApps = {
 	'Firefox': ['Legújabb', 'firefox.zip'],
 	'VLC': ['3.0.11', 'offline-vlc-3.0.11-win64.exe'],
 	'Office': ['2016', 'offline-office-2016.exe'],
-	'ESET Copier': ['12.31', 'ESET-Copier-20.1231.zip'],
+	'ESET Copier': ['05.09', 'ESET-Copier-21.0509.zip'],
 	'ESET Smart Security': ['Legújabb', 'eset.zip']
 };
 
