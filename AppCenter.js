9605d01eb9834294978bdb171905c07a678f6261
re-is <33784291+re-is@users.noreply.github.com>
2024-04-16 08:22:31 +0200

    Update AppCenter.js

--- AppCenter.js
+++ AppCenter.js
@@ -14,7 +14,7 @@ window.usbApps = {
 };
 
 window.webApps = {
-	'Activator'						: { version: '24.04.04',	name: 'Activator-04.04.zip' },
+	'Activator'						: { version: '24.04.16',	name: 'Activator-04.16.zip' },
 	'Any To Icon'					: { version: '12.26',		name: 'Any-12.26.zip' },
 	'AutoIt'						: { version: '23.08.28',	name: 'AutoIt-08.28.zip' },
 	'CCleaner'						: { version: '5.83',		name: 'CCleaner.09.08.zip' },
