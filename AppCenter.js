05e8a0e2a1da94d6d573a29b22f060695b7b2c10
re-is <33784291+re-is@users.noreply.github.com>
2024-04-20 19:12:55 +0200

    Update AppCenter.js

--- AppCenter.js
+++ AppCenter.js
@@ -1,48 +1,55 @@
 window.usbApps = {
-	'Nvidia GeForce'				: { version: 'Legújabb',	name: 'geforce.bat' },
+	'Nvidia GeForce'		: { version: 'Legújabb',	name: 'geforce.bat' },
 	'Acer laptop, Intel HD 5500'	: { version: '15.40',		name: 'Acer laptop\\win64_15.40.5171.exe' },
-	'Acer laptop, NVIDIA'			: { version: '537.13',		name: 'Acer laptop\\537.13-notebook-win10-win11-64bit-international-dch-whql.exe' },
-	'HP laptop, Intel Graphics'		: { version: '100.3370',	name: 'HP laptop\\IntelGraphicsExperience_1.100.3370.AppxBundle' },
-	'HP laptop, Intel Iris X'		: { version: '100.9864',	name: 'HP laptop\\igfx_win_100.9864.exe' },
-	'VLC'							: { version: '3.0.18',		name: 'vlc-3.0.18-win64.exe' },
-	'Office 21'						: { version: '2021',		name: 'Office-2021\\installer.bat' },
-	'MEGA'							: { version: 'Legújabb',	name: 'MEGAsyncSetup64.exe' },
-	'Firefox'						: { version: 'Legújabb',	name: 'Browsers\\Firefox Installer.exe' },
-	'Chrome'						: { version: 'Legújabb',	name: 'Browsers\\ChromeSetup.exe' },
-	'uTorrent'						: { version: '2.2',			name: 'utorrent_2.2.exe' },
-	'ESET Smart Security'			: { version: 'Legújabb',	name: 'eset_smart_security_premium_live_installer.exe' }
+	'Acer laptop, NVIDIA'		: { version: '537.13',		name: 'Acer laptop\\537.13-notebook-win10-win11-64bit-international-dch-whql.exe' },
+	'HP laptop, Intel Graphics'	: { version: '100.3370',	name: 'HP laptop\\IntelGraphicsExperience_1.100.3370.AppxBundle' },
+	'HP laptop, Intel Iris X'	: { version: '100.9864',	name: 'HP laptop\\igfx_win_100.9864.exe' },
+	'VLC'				: { version: '3.0.18',		name: 'vlc-3.0.18-win64.exe' },
+	'Office 21'			: { version: '2021',		name: 'Office-2021\\installer.bat' },
+	'MEGA'				: { version: 'Legújabb',	name: 'MEGAsyncSetup64.exe' },
+	'Firefox'			: { version: 'Legújabb',	name: 'Browsers\\Firefox Installer.exe' },
+	'Chrome'			: { version: 'Legújabb',	name: 'Browsers\\ChromeSetup.exe' },
+	'uTorrent'			: { version: '2.2',		name: 'utorrent_2.2.exe' },
+	'ESET Smart Security'		: { version: 'Legújabb',	name: 'eset_smart_security_premium_live_installer.exe' }
 };
 
 window.webApps = {
-	'Activator'						: { version: '24.04.16',	name: 'Activator-04.16.zip' },
-	'Any To Icon'					: { version: '12.26',		name: 'Any-12.26.zip' },
-	'AutoIt'						: { version: '23.08.28',	name: 'AutoIt-08.28.zip' },
-	'CCleaner'						: { version: '5.83',		name: 'CCleaner.09.08.zip' },
-	'DiskInfo'						: { version: '8.12.8',		name: 'DiskInfo-08.28.zip' },
-	'Defrag'						: { version: '12.26',		name: 'Defrag-12.26.zip' },
-	'WinPlus'						: { version: '11.18',		name: 'WinPlus-11.18.zip' },
-	'Photo Filter 10'				: { version: '12.26',		name: 'Photo-12.26.zip' },
-	'Resource Hacker'				: { version: '12.26',		name: 'ResHacker-12.26.zip' },
-	'Sublime Text 4169'				: { version: '24.04.16',	name: 'Sublime-4169-04.16.zip' },
-	'Volumouse'						: { version: '08.28',		name: 'Volumouse-08.28.zip' },
-	'WinRAR'						: { version: '12.26',		name: 'Win-Rar-12.26.zip' },
-	'WinSCP'						: { version: '23.08.28',	name: 'WinSCP-08.28.zip' },
-	'WU Switcher'					: { version: '24.04.18',	name: 'WU-switcher-04.18.zip' },
-	'Fonter'						: { version: '23.09.15',	name: 'Fonter-09.15.zip' },
-	'Context Editor'				: { version: '22.05.12',	name: 'Context-Editor.zip' }
+	'Activator'			: { version: '24.04.16',	name: 'Activator-04.16.zip' },
+	'Any To Icon'			: { version: '12.26',		name: 'Any-12.26.zip' },
+	'AutoIt'			: { version: '23.08.28',	name: 'AutoIt-08.28.zip' },
+	'CCleaner'			: { version: '5.83',		name: 'CCleaner.09.08.zip' },
+	'DiskInfo'			: { version: '8.12.8',		name: 'DiskInfo-08.28.zip' },
+	'Defrag'			: { version: '12.26',		name: 'Defrag-12.26.zip' },
+	'WinPlus'			: { version: '11.18',		name: 'WinPlus-11.18.zip' },
+	'Photo Filter 10'		: { version: '12.26',		name: 'Photo-12.26.zip' },
+	'Resource Hacker'		: { version: '12.26',		name: 'ResHacker-12.26.zip' },
+	'Sublime Text 4169'		: { version: '24.04.16',	name: 'Sublime-4169-04.16.zip' },
+	'Volumouse'			: { version: '08.28',		name: 'Volumouse-08.28.zip' },
+	'WinRAR'			: { version: '12.26',		name: 'Win-Rar-12.26.zip' },
+	'WinSCP'			: { version: '23.08.28',	name: 'WinSCP-08.28.zip' },
+	'WU Switcher'			: { version: '24.04.20',	name: 'WU-switcher-04.20.zip' },
+	'Fonter'			: { version: '23.09.15',	name: 'Fonter-09.15.zip' },
+	'Context Editor'		: { version: '22.05.12',	name: 'Context-Editor.zip' }
 };
 
 window.checkList = '\r\n\
-   Windows telepítési sorrend\r\n\r\n\
-   1) Eszköznév megadásakor: ipconfig /release\r\n\
-   2) Fióknév megadásakor: ipconfig /renew\r\n\r\n\
-   3) Edge megnyitása és beállítása\r\n\
-   4) Mobil "Adatforgalom" átállítása korlátlanra, különben nem fogja letölteni a nagy fájlokat\r\n\
-   5) Windows Update futtatása\r\n\r\n\
-   6) USB-n lévő Runas.bat futtatása Adminként\r\n\
-   7) Office 2021 és a Fiók menüből frissíteni, utána kikapcsolni\r\n\
-   8) Windows Update kikapcsolása, SoftwareDistribution törlése\r\n\
-   9) Tisztogatás\r\n\
-   10) Aktiválás\r\n\r\n\
-   Értesítések és auto defrag kikapcsolása\r\n\
-   Energiaellátás beállítása';
+   Windows telepítési sorrend\r\n\
+	\r\n\
+   Win11: Eszköznév megadásakor: ipconfig /release\r\n\
+   Win11: Fióknév megadásakor: ipconfig /renew\r\n\
+	\r\n\
+   -> Edge megnyitása és beállítása\r\n\
+   -> Mobil "Adatforgalom" átállítása korlátlanra, különben nem fogja letölteni a nagy fájlokat\r\n\
+   -> Windows Defender Gyorsteszt futtatása, amíg kevés fájl van a gépen !\r\n\
+   -> Windows Update futtatása\r\n\
+	\r\n\
+   -> USB-n lévő Runas.bat futtatása Adminként\r\n\
+   -> Office 2021 és a Fiók menüből frissíteni, utána kikapcsolni\r\n\
+   -> Windows Update kikapcsolása, SoftwareDistribution törlése ( C: -> Tulajdonságok -> Lemezkarbantartó )\r\n\
+   -> Tisztogatás\r\n\
+   -> Aktiválás\r\n\
+	\r\n\
+   -> Chrome telepítése után, Egyebek mappában lévő .reg fájl hozzáadása\r\n\
+   -> Fájltársítások kipróbálása\r\n\
+   -> Értesítések és auto defrag kikapcsolása\r\n\
+   -> Energiaellátás beállítása';
